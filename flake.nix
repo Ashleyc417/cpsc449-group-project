8e364f1bcf70e69ee74b749692e2f5c57c14d2ab
Mark Gaballa <mgaballa@csu.fullerton.edu>
2026-04-13 20:13:30 -0700

    init

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+	description = "Flake for dotnet backend project";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs";
+		flake-utils.url = "github:numtide/flake-utils";
+	};
+
+	outputs = { self, nixpkgs, flake-utils}: 
+	flake-utils.lib.eachDefaultSystem(system:
+	let
+		pkgs = nixpkgs.legacyPackages.${system};
+	in {
+		devShell = pkgs.mkShell {
+				buildInputs = with pkgs; [
+						#dotnet-aspnetcore
+						#dotnet-runtime
+					dotnet-sdk_8
+				];
+
+			shellHook = ''
+				export DOTNET_ROOT=${pkgs.dotnet-sdk_8}/share/dotnet/;
+			'';
+		};
+	}
+	);
+}
