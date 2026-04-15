e93c679f05c7605ca05c59ecd22f0df5518b091d
Ashley Chan <ashleyc417@csu.fullerton.edu>
2026-04-15 14:30:00 -0700

    fix model bugs in Book, Member, and BorrowRecord, add .gitignore

--- lendify/Models/BorrowRecord.cs
+++ lendify/Models/BorrowRecord.cs
@@ -1,4 +1,4 @@
-namespace lendify.Modesl;
+namespace lendify.Models;
 
 public enum BorrowStatus 
 {
@@ -12,6 +12,6 @@ public class BorrowRecord
 	public Guid BookId { get; set; } // Guid for book id? I Might be wrong
 	public Guid MemberId { get; set; } // same here
 	public DateTime BorrowDate { get; set; }
-	public DateTime ReturnDate { get; set; }
+	public DateTime? ReturnDate { get; set; }
 	public BorrowStatus Status { get; set; }
 }
--- lendify/Models/Member.cs
+++ lendify/Models/Member.cs
@@ -2,7 +2,7 @@ namespace lendify.Models;
 
 public class Member
 {
-	public Guid Id { get; set; };
+	public Guid Id { get; set; }
 	public string FullName { get; set; } = string.Empty;
 	public string Email { get; set; } = string.Empty;
 	public DateTime MembershipDate { get; set; }
