620d5f0746f62dc08e5d01c7678f10bee12b9307
Mark Gaballa <mgaballa@csu.fullerton.edu>
2026-04-15 18:34:55 -0700

    remove obj directory, use gitignore, add simple ci

--- lendify/Models/BorrowRecord.cs
+++ lendify/Models/BorrowRecord.cs
@@ -1,17 +1,17 @@
 namespace lendify.Modesl;
 
-public enum BorrowStatus 
+public enum BorrowStatus
 {
-	Borrowed,
-	Returned
+    Borrowed,
+    Returned
 }
 
 public class BorrowRecord
 {
-	public Guid Id { get; set; }
-	public Guid BookId { get; set; } // Guid for book id? I Might be wrong
-	public Guid MemberId { get; set; } // same here
-	public DateTime BorrowDate { get; set; }
-	public DateTime ReturnDate { get; set; }
-	public BorrowStatus Status { get; set; }
+    public Guid Id { get; set; }
+    public Guid BookId { get; set; } // Guid for book id? I Might be wrong
+    public Guid MemberId { get; set; } // same here
+    public DateTime BorrowDate { get; set; }
+    public DateTime ReturnDate { get; set; }
+    public BorrowStatus Status { get; set; } = BorrowStatus.Returned;
 }
