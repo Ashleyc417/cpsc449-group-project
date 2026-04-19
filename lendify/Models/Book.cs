56663d445bcc48be8983e98f0ca4b7da2df57674
Gaballa <mgaballa@csu.fullerton.edu>
2026-04-18 19:30:20 -0700

    Cleanup and ci (#2)
    * remove obj directory, use gitignore, add simple ci
    
    * fix build
    
    * typo
    
    * remove obj
    
    * add make fmt
    
    * comment

--- lendify/Models/Book.cs
+++ lendify/Models/Book.cs
@@ -2,10 +2,10 @@ namespace lendify.Models;
 
 public class Book
 {
-	public Guid Id { get; set; }
-	public string Title { get; set; } = string.Empty;
-	public string Author { get; set; } = string.Empty;
-	public int Isbn { get; set; } = 0;
-	public int TotalCopies { get; set; } = 0;
-	public int AvailableCopies { get; set; } = 0;
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public int Isbn { get; set; } = 0;
+    public int TotalCopies { get; set; } = 0;
+    public int AvailableCopies { get; set; } = 0;
 }
