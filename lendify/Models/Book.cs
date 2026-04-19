de2542288d7f093f714f5ee59e07af53a1749901
Ashley Chan <ashleyc417@csu.fullerton.edu>
2026-04-18 23:17:23 -0700

    fix model bugs in Book, Member, and BorrowRecord, add .gitignore

--- lendify/Models/Book.cs
+++ lendify/Models/Book.cs
@@ -5,7 +5,7 @@ public class Book
 	public Guid Id { get; set; }
 	public string Title { get; set; } = string.Empty;
 	public string Author { get; set; } = string.Empty;
-	public int Isbn { get; set; } = 0;
+	public string Isbn { get; set; } = string.Empty;
 	public int TotalCopies { get; set; } = 0;
 	public int AvailableCopies { get; set; } = 0;
 }
