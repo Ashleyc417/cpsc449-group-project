37620426ca33b3ab16759623ae3204311837e6fe
Gaballa <mgaballa@csu.fullerton.edu>
2026-04-18 19:30:20 -0700

    Cleanup and ci (#2)
    * remove obj directory, use gitignore, add simple ci
    
    * fix build
    
    * typo
    
    * remove obj
    
    * add make fmt
    
    * comment

--- lendify/Models/BorrowRecord.cs
+++ lendify/Models/BorrowRecord.cs
@@ -1,17 +1,19 @@
-namespace lendify.Modesl;
+namespace lendify.Models;
 
-public enum BorrowStatus 
+// Could probably just be a bool for optimising 
+// if just two enum values
+public enum BorrowStatus
 {
-	Borrowed,
-	Returned
+    Borrowed,
+    Returned
 }
 
 public class BorrowRecord
 {
-	public Guid Id { get; set; }
-	public Guid BookId { get; set; } // Guid for book id? I Might be wrong
-	public Guid MemberId { get; set; } // same here
-	public DateTime BorrowDate { get; set; }
-	public DateTime ReturnDate { get; set; }
-	public BorrowStatus Status { get; set; }
+    public Guid Id { get; set; }
+    public Guid BookId { get; set; } // Guid for book id? I Might be wrong
+    public Guid MemberId { get; set; } // same here
+    public DateTime BorrowDate { get; set; }
+    public DateTime ReturnDate { get; set; }
+    public BorrowStatus Status { get; set; } = BorrowStatus.Returned;
 }
