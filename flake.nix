afa916b8c8a32f403030c003844c4ae7419c7927
Gaballa <lGaballal@yahoo.com>
2026-04-25 15:24:15 -0700

    remove shellHook from flake

--- flake.nix
+++ flake.nix
@@ -1,28 +1,28 @@
 {
-	description = "Flake for dotnet backend project";
+  description = "Flake for dotnet backend project";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs";
-		flake-utils.url = "github:numtide/flake-utils";
-	};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-	outputs = { self, nixpkgs, flake-utils}: 
-	flake-utils.lib.eachDefaultSystem(system:
-	let
-		pkgs = nixpkgs.legacyPackages.${system};
-	in {
-		devShell = pkgs.mkShell {
-				buildInputs = with pkgs; [
-						#dotnet-aspnetcore
-						#dotnet-runtime
-					dotnet-sdk_8
-					act
-				];
-
-			shellHook = ''
-				export DOTNET_ROOT=${pkgs.dotnet-sdk_8}/share/dotnet/;
-			'';
-		};
-	}
-	);
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            #dotnet-aspnetcore
+            #dotnet-runtime
+            dotnet-sdk_8
+            act
+          ];
+        };
+      }
+    );
 }
